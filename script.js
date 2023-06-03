5208a34b0e20213c1d8d0769e10ccb641bf412b5
kiara <kiarahoheb@gmail.com>
2023-06-03 18:34:49 -0400

    worked on opeartors functionality

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 function Calculator() {}
 Calculator.prototype = {
   constructor: Calculator,
+  enableOperators : function(){
+    operators.forEach(op => op.disabled = false);
+  },
+  disableOperators : function(){
+    operators.forEach(op => op.disabled = true)
+  },
+  nonDisableNumbers : function(){
+    numbers.forEach(numBtn => {
+      numBtn.disabled = false
+    })
+  },
   clear: function () {
     total.textContent = 0;
     decimal.disabled = false;
     del.disabled = false;
+    this.nonDisableNumbers()
+    this.enableOperators()
   },
   percent: function () {
-    total.textContent = parseInt(total.textContent) / 100
-    },
+    total.textContent = parseInt(total.textContent) / 100;
+  },
   plusNegative: function () {
     total.textContent *= -1;
   },
@@ -17,6 +30,8 @@ Calculator.prototype = {
     decimal.disabled = true;
   },
   delete: function () {
+    this.nonDisableNumbers()
+    this.enableOperators()
     if (total.textContent.length === 1) {
       total.textContent = 0;
       del.disabled = true;
@@ -24,39 +39,38 @@ Calculator.prototype = {
       del.disabled = false;
       total.textContent = total.textContent.slice(0, -1);
     }
+    console.log(total.textContent)
   },
   enableDeleteButton: function () {
     del.disabled = false;
   },
-  equal : function(){
-    const arr = total.textContent.split(' ')
-  for(let i = 0; i < arr.length; i++){
-    if(arr[i] === '+'){
-      let bo = total.textContent.split(' ')
-      console.log(bo.length)
-      if(bo.length === 3){
-      const test = parseFloat(arr[i - 1]) + parseFloat(arr[i + 1])
-      total.textContent = test
-      }else if(bo.length > 3){
-        // [3,+,3,*,3]
-        // if there's more than one operator 
-        // calc the first three values: 3 + 3
-        // then take that value and calc it by remaining values
+  equal: function () {
+    // splits the total.textContent at the operators while still including the operator in the array using ()- capture group
+    let regex = /([+\-*/])/
+    const arr = total.textContent.split(regex);
+    //console.log(arr.length)
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === "+") {
+        // console.log(arr);
+        if (arr.length === 3) {
+          const test = parseFloat(arr[i - 1]) + parseFloat(arr[i + 1]);
+          total.textContent = test;
+        }
+      } else if (arr[i] === "-") {
+        const test = parseFloat(arr[i - 1]) - parseFloat(arr[i + 1]);
+        total.textContent = test;
+      } else if (arr[i] === "/") {
+        const test = parseFloat(arr[i - 1]) / parseFloat(arr[i + 1]);
+        total.textContent = test;
       }
-    }else if(arr[i] === '-'){
-      const test = parseFloat(arr[i - 1]) - parseFloat(arr[i + 1])
-      total.textContent = test
-    }else if(arr[i] === '/'){
-      const test = parseFloat(arr[i - 1]) / parseFloat(arr[i + 1])
-      total.textContent = test
-    }if(arr[i] === '*'){
-      const test = parseFloat(arr[i - 1]) * parseFloat(arr[i + 1])
-      total.textContent = test
+      if (arr[i] === "*") {
+        const test = parseFloat(arr[i - 1]) * parseFloat(arr[i + 1]);
+        total.textContent = test;
+      }
     }
-  }
-  }
+  },
 };
-
+const calculator = new Calculator();
 const total = document.querySelector(".total");
 const clear = document.querySelector(".btn-clear");
 const percent = document.querySelector(".btn-percent");
@@ -65,35 +79,59 @@ const button = document.querySelectorAll("button");
 const del = document.querySelector(".delete");
 const decimal = document.querySelector(".btn-decimal");
 const zero = document.querySelector(".btn-0");
-const numbers = document.querySelector(".num")
-const calculator = new Calculator();
+const numbers = document.querySelectorAll(".num");
 const plus = document.querySelector(".btn-plus");
 const equal = document.querySelector(".btn-equal");
-
-// let text = total.textContent.length;
-// let regex = /^.{1,11}$/;
-// if(!regex.test(text)){
-//     numbers.disabled = true
-//     console.log('regex')
-// }
+const operators = document.querySelectorAll(".operators")
 
 total.textContent = 0;
 
+// disables operators after an operator is clicked
+operators.forEach(op => {
+  op.addEventListener("click",(event) => {
+  calculator.disableOperators()
+  equal.disabled = true
+  calculator.equal()
+  })
+})
+
+// enables the operators when a number is clicked
+numbers.forEach(numBtn => {
+  numBtn.addEventListener("click",(event) => {
+    calculator.enableOperators()
+    equal.disabled = false
+  })
+})
+
 button.forEach((btn) => {
   btn.addEventListener("click", (event) => {
+    // if total length is >= 10 disable numBtn(all number buttons)
+    if (total.textContent.length >= 10) {
+      numbers.forEach(numBtn => {
+        numBtn.disabled = true;
+        //calculator.nonDisableNumbers()
+      })
+    }
+    // total is set to the btn val (if button 1 & 2 is clicked total will display 12)
     total.textContent += btn.value;
+
+    // making sure total div is never empty
     if (total.textContent.length > 1) {
       calculator.enableDeleteButton();
     }
-    
+
+    // making sure there's no leading 0's
     if (
       total.textContent.length > 1 &&
       total.textContent[0] === "0" &&
       total.textContent[1] !== "."
     ) {
+      // disable zero if there's more than one without a '.' in the beginning of the total like: '001 || 0000234'
       zero.disabled = true;
+      // remove the leading zero
       total.textContent = total.textContent.substring(1);
     } else if (
+      // if theres a '.' enable the 0 btn
       total.textContent.length === 2 &&
       total.textContent[0] === "0" &&
       total.textContent[1] === "."
@@ -117,4 +155,4 @@ percent.addEventListener("click", (event) => calculator.percent());
 plusNegative.addEventListener("click", (event) => calculator.plusNegative());
 decimal.addEventListener("click", (event) => calculator.handleClick());
 del.addEventListener("click", (event) => calculator.delete());
-equal.addEventListener("click",(event) => calculator.equal())
+equal.addEventListener("click", (event) => calculator.equal());

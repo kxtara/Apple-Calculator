1e04730f62d1db03772e3b5f0439f6bd211067bc
kiara <kiarahoheb@gmail.com>
2023-06-04 19:12:49 -0400

    testing js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/**/*.{html,js}"],
+  content: ["./build/*.html", "./build/js/*.js"],
   theme: {
     extend: {
       colors:{
